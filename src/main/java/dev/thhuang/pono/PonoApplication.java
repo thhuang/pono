84c40acce435c29a641ed24a3974d02479de10a6
TH Huang <tzhsn.huang@gmail.com>
2023-05-31 19:01:28 -0500

    [arch] initialized

---
+++ src/main/java/dev/thhuang/pono/PonoApplication.java
@@ -0,0 +1,13 @@
+package dev.thhuang.pono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PonoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/thhuang/pono/PonoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thhuang.pono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
