2fae60fdd955f29bdebac96bfe2d76cd07ef0cbd
TH Huang <tzhsn.huang@gmail.com>
2023-06-05 16:45:25 -0500

    [app] added saide

---
+++ apps/saide/src/main/java/dev/thhuang/saide/SaideApplication.java
@@ -0,0 +1,13 @@
+package dev.thhuang.saide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaideApplication.class, args);
+	}
+
+}
---
+++ apps/saide/src/test/java/dev/thhuang/saide/SaideApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thhuang.saide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
